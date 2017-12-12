bfd4f4f0d0cce4df1b2e058d750fb4c4b3a3874b
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:06:47 +0100

    fini

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 3</title>
+  </head>
+  <body>
+    <?php
+    $second = 10;
+    $first = 100;
+    while($first >= 10){
+      $first--;
+      $result = ($first * $second);
+      echo $result.'<br />';
+    }
+     ?>
+  </body>
+</html>
